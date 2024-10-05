352cfe272f84a930180461535e5233a9dc381f87
Anna Bieliavtseva <belyavtseva1911@gmail.com>
2024-10-05 20:42:31 +0200

    finish all task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm (event) {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+    if ( email === "" ||  password === "") {
+        return alert("All fields must be filled in.")
+    }
+    
+    const dataLogin = {email, password}
+    console.log(dataLogin)
+    loginForm.reset()
+    
+}
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,40 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  input: document.querySelector('#controls input'),
+  btnCreate: document.querySelector('[data-create]'),
+  btnDestroy: document.querySelector('[data-destroy]'),
+  boxes: document.getElementById('boxes')
+}
+
+function createBoxes(amount) {
+const elements = []
+let size = 30;
+for (let i = 1; i <= amount; i++) {
+  const markup = document.createElement('div');
+  markup.style.width = `${size}px`;
+  markup.style.height = `${size}px`;
+  markup.style.marginTop = '10px'
+  markup.style.backgroundColor = getRandomHexColor();
+  elements.push(markup);
+  size+=10;
+}
+refs.boxes.append(...elements);
+}
+
+ refs.btnCreate.addEventListener('click', ()=> {
+  const inputValue = Number(refs.input.value);
+  createBoxes(inputValue);
+}
+)
+
+function destroyBoxes() {
+  refs.boxes.innerHTML = '';
+  refs.input.value = 0;
+}
+
+refs.btnDestroy.addEventListener('click', destroyBoxes)
+
+
